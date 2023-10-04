3446a270e293aef4dfdffcd6a2e5d5399c97d1ef
Roger Padrell Casar <143524580+roger-padrell@users.noreply.github.com>
2023-10-04 14:25:17 +0200

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,17 +1,17 @@
 const FIGMA_API_KEY = 'figd_-6qbnuP5kExseVf3evYD9pvlj2rIfc1hTGSrDAiU';
 const FIGMA_FILE_ID = '0t9SMmG2b9CsmIs32UgCfN';
 
-const getFigmaFile = async () => {
+const getFigmaFile = function() {
   try {
-    const response = await fetch(
-      `https://api.figma.com/v1/files/${FIGMA_FILE_ID}`,
-      {
-        headers: {
-          'X-Figma-Token': FIGMA_API_KEY,
-        },
-      }
-    );
-    console.log(response);
+    fetch(`https://api.figma.com/v1/files/${FIGMA_FILE_ID}`, {
+		method: 'GET',
+		headers: {
+			'X-Figma-Token': FIGMA_API_KEY,
+		},
+	})
+		.then(response => response.json())
+		.then(response => console.log(response))
+    
   } catch (error) {
     console.error(error);
   }
