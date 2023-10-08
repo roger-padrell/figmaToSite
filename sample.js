1346ba7fb37e51b2f057f5421940bd1dedbdc25f
Roger Padrell Casar <143524580+roger-padrell@users.noreply.github.com>
2023-10-08 16:50:07 +0200

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,5 +1,4 @@
 var FIGMA_API_KEY = 'vehrsN3yZVv8zXt82g8Jm0CiLDlRM8';
-const FIGMA_FILE_ID = prompt("Write the file ID:", '0t9SMmG2b9CsmIs32UgCfN');
 
 var db = {};
 db.data = {};
@@ -31,7 +30,7 @@ function loadVars(response){
 
 const getFigmaFile = function() {
   try {
-    fetch(`https://api.figma.com/v1/files/${FIGMA_FILE_ID}`, {
+    fetch("https://api.figma.com/v1/files/" + prompt("Write the file ID:", '0t9SMmG2b9CsmIs32UgCfN'), {
 		method: 'GET',
 		headers: {
 			'X-Figma-Token': FIGMA_API_KEY,
